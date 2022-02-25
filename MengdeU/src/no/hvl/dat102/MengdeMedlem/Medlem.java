058a31dc595e64391e07d6c3694d76c5e914d5d9
h591099 <89256511+591099T@users.noreply.github.com>
2022-02-25 17:30:17 +0100

    n

--- MengdeU/src/no/hvl/dat102/MengdeMedlem/Medlem.java
+++ MengdeU/src/no/hvl/dat102/MengdeMedlem/Medlem.java
@@ -7,74 +7,7 @@ import no.hvl.dat102.mengde.kjedet.KjedetMengde;
 
 public class Medlem extends KjedetMengde {
 
-	private String navn;
-	private MengdeADT<Hobby> hobbyer;
-	private int statusIndeks;
-	// … Konstruktør
 
-	public Medlem() {
-
-	}
-
-	public Medlem(String navn, MengdeADT<Hobby> hobbyer) {
-		this.navn = navn;
-		this.hobbyer = new KjedetMengde<Hobby>();
-		
-	}
-
-	public String getNavn() {
-		return navn;
-	}
-
-	public MengdeADT<Hobby> getHobbyer() {
-		return hobbyer;
-	}
-
-	public int getStatusIndeks() {
-		return statusIndeks;
-	}
-
-	public void setNavn(String navn) {
-		this.navn = navn;
-	}
-
-	public void setHobbyer(MengdeADT<Hobby> hobbyer) {
-		this.hobbyer = hobbyer;
-	}
-
-	public void setStatusIndeks(int statusIndeks) {
-		this.statusIndeks = statusIndeks;
-	}
-
-	public boolean passerTil(Medlem medlem2) {
-		// Har dei nøyaktig samme hobbyane.
-		// (Like mengder)
-		boolean passer = false;
-		MengdeADT<Hobby> midler = this.getHobbyer();
-		MengdeADT<Hobby> midler2 = medlem2.getHobbyer();
-		if (midler == midler2) {
-			passer = true;
-		}
-		if (midler2 == null) {
-			passer = false;
-		}
-		if (midler.getClass() != midler2.getClass()) {
-			passer = false;
-		}
-		return passer;
-		// return hobbyer.equals(medlem2.getHobbyer());
-	}
-
-	// … Andre metoder
-
-	public void skrivUt() {
-		Iterator<Hobby> t = hobbyer.iterator();
-		System.out.println("Navn: " + navn);
-		while (t.hasNext()) {
-			System.out.println(t.next().toString());
-		}
-		System.out.println("Status: " + statusIndeks);
-
-	}
+	
 
 }
